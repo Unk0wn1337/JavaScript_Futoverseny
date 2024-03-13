c25c7c666c1aa9d5322f06e0a1bb54c180dac2b7
Unk0wn1337 <kelogames@gmail.com>
2024-03-13 13:46:02 +0100

    verseny kesz

--- index.js
+++ index.js
@@ -41,20 +41,41 @@ function osszesit(lista) {
 }
 
 // 3. feladat
-function befutott(sor, adatok) {
-  
-    
+function befutott(adatok) {
+    const HARMADIK = document.getElementById("feladat_3");
+    const SOR = document.querySelectorAll("tr");
+    for (let index = 0; index < SOR.length; index++) {
+        SOR[index].addEventListener("click",function(event){
+            if(index >= 0 && index <= 8 && !befutottVersenyzok.includes(SOR[index])){
+                console.log(event.target,index);
+                SOR[index].classList.add("befutott");
+                HARMADIK.innerHTML += SOR[index].innerHTML;
+                befutottVersenyzok.push(SOR[index]);   
+                ellenoriz();   
+            }
+        });
+    }
+         
 }
 
 // 4. feladat
 function torolEsemeny() {
-
+    const HARMADIK = document.getElementById("feladat_3");
+    const TORLES_BTN = document.querySelector("#options button");
+    TORLES_BTN.addEventListener("click",function(){
+        HARMADIK.classList.remove("befutott"); 
+        HARMADIK.innerHTML = ``;
+    });
 }
 
 // 5. feladat
-function ellenoriz(){
-  
+function ellenoriz(){   
+if(befutottVersenyzok.length>=8){
+    alert()
+   }
 }
 
 letrehozTablazat(FUTOK);
 osszesit(FUTOK);
+befutott(FUTOK);
+torolEsemeny();
